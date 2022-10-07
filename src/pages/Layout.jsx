14983732f65488def47a67ff8aebcb85d7d91e13
Clinton Rizzo <70792821+clintrizzo@users.noreply.github.com>
2022-10-07 13:27:10 -0700

    folder structure updated / css for footer has been updated

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import {Outlet} from "react-router-dom";
-import Navbar from "../NavbarHome.jsx";
+import Navbar from "../components/navbar/NavbarHome";
 
 const Layout = () => {
   return (
